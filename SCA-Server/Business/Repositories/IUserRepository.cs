1c6985f1dd521b27cc83896e2b502120662cb60f
Allertvdw <allertvdw@hotmail.com>
2024-12-17 11:42:48 +0100

    Added elo logic

--- SCA-Server/Business/Repositories/IUserRepository.cs
+++ SCA-Server/Business/Repositories/IUserRepository.cs
@@ -5,6 +5,7 @@ namespace Business.Repositories;
 public interface IUserRepository
 {
     User CreateUser(User user);
+    User UpdateUser(User user);
     User? GetUserById(int id);
     User? GetUserByEmail(string email);
     List<User> GetAllUsers();
