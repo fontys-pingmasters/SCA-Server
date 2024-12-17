a9103d8fffa631e19ed2b83d8b249409b4811bf0
Allertvdw <allertvdw@hotmail.com>
2024-12-17 12:23:53 +0100

    Added elo property to UserDto

--- SCA-Server/Business/Dtos/EntityDtos/UserDto.cs
+++ SCA-Server/Business/Dtos/EntityDtos/UserDto.cs
@@ -7,6 +7,7 @@ public class UserDto
     public int Id { get; set; }
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
+    public int Elo { get; set; }
     public string Email { get; set; } = string.Empty;
     public Roles Role { get; set; }
 }
