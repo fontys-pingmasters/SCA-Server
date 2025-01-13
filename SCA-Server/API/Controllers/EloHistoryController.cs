7cb74fa075654ce32afa8db3b3d51260d6d614e1
Allertvdw <allertvdw@hotmail.com>
2025-01-13 10:48:11 +0100

    Added GetAllElohistoriesByUserId endpoint

---
+++ SCA-Server/API/Controllers/EloHistoryController.cs
@@ -0,0 +1,29 @@
+﻿using Business.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace SCA_Server.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class EloHistoryController : ControllerBase
+	{
+		private readonly IEloHistoryService _eloHistoryService;
+
+		public EloHistoryController(IEloHistoryService eloHistoryService)
+		{
+			_eloHistoryService = eloHistoryService;
+		}
+
+		[HttpGet]
+		public IActionResult GetAllEloHistoriesByUserId()
+		{
+			var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier) ??
+			throw new Exception("Could not find current user id in token"));
+
+			var eloHistory = _eloHistoryService.GetAllEloHistoriesByUserId(userId);
+			return Ok(eloHistory);
+		}
+	}
+}
---
+++ SCA-Server/Business/Implementations/EloHistoryService.cs
@@ -0,0 +1,26 @@
+﻿using Business.Entities;
+using Business.Repositories;
+using Business.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Implementations
+{
+	public class EloHistoryService : IEloHistoryService
+	{
+		IEloHistoryRepository _eloHistoryRepository;
+
+		public EloHistoryService(IEloHistoryRepository eloHistoryRepository)
+		{
+			_eloHistoryRepository = eloHistoryRepository;
+		}
+
+		public List<EloHistory> GetAllEloHistoriesByUserId(int userId)
+		{
+			return _eloHistoryRepository.GetAllEloHistoriesByUserId(userId);
+		}
+	}
+}
